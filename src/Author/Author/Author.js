bfbabad57649ec074a6c6d493f8aa21aae97fc1f
Anderson Alves <andersonalves4608@gmail.com>
2019-09-25 18:58:18 -0300

    Iniciando front react

---
+++ src/Author/Author/Author.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Button, Container, Image, Row, Col, Table, Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import logo from "../../Img/logo-ex-2.png";
+
+class Author extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+    componentDidMount() {
+        let author_id = this.props.match.params.id;
+        fetch(`http://localhost:8000/v1/author/${author_id}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <div>
+                        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                            <Navbar.Brand href="/">Home</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mr-auto">
+                                    <NavDropdown title="Author" id="nav_author">
+                                        <NavDropdown.Item href="/new/author">New</NavDropdown.Item>
+                                        <NavDropdown.Item href="/author">List</NavDropdown.Item>
+                                    </NavDropdown>
+                                    <NavDropdown title="Book" id="nav_book">
+                                        <NavDropdown.Item href="/new/book">New</NavDropdown.Item>
+                                        <NavDropdown.Item href="/book">List</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
+                    </div>
+                    <div>
+                        <Container>
+                            <Row className={"justify-content-center"}>
+                                <Col  xs={6} md={4}>
+                                    <Image src={logo} roundedCircle />
+                                </Col>
+                            </Row>
+                        </Container>
+                        <Container>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th colSpan={3}>Opções</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr key={items.name}>
+                                        <td>{items.id}</td>
+                                        <td>{items.name}</td>
+                                        <td><Button variant={"secondary"} size="lg" block>Alterar</Button></td>
+                                        <td><Button variant={"secondary"} href={`/del/author/${items.id}`} size="lg" block>Excluir</Button></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Container>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+export default Author;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import AuthorList from "./Author/List_authors/Author-list";
+import Author from "./Author/Author/Author";
+import AuthorNew from "./Author/New_author/Author-new";
+import AuthorRemove from "./Author/Remove_author/Author-remove";
+import Booklist from "./Book/List_books/Book-list";
+import BookNew from "./Book/New_book/Book-new";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/author" exact={true} component={AuthorList} />
+            <Route path="/author/:id" exact={true} component={Author} />
+            <Route path="/new/Author" exact={true} component={AuthorNew} />
+            <Route path="/del/Author/:id" exact={true} component={AuthorRemove} />
+            <Route path="/Book" exact={true} component={Booklist} />
+            <Route path="/new/Book" exact={true} component={BookNew} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
