9d4276b2a5af8bc6b1aa14198a426d794d86fa37
Anderson Alves <andersonalves4608@gmail.com>
2019-09-26 16:15:23 -0300

    Alterando tela da home para menu de busca

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Img/logo-ex-2.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  Container,
+  Image,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  InputGroup,
+  FormControl,
+  Dropdown,
+  DropdownButton
+} from 'react-bootstrap'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <NavDropdown title="Author" id="nav_author">
+                  <NavDropdown.Item href="/new/author">New</NavDropdown.Item>
+                  <NavDropdown.Item href="/author">List</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Book" id="nav_book">
+                  <NavDropdown.Item href="/new/book">New</NavDropdown.Item>
+                  <NavDropdown.Item href="/book">List</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+        <div>
+          <Container>
+            <Row className={"justify-content-center"}>
+              <Col  xs={6} md={4}>
+                <Image src={logo} roundedCircle />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div>
+          <InputGroup>
+            <FormControl
+                placeholder="Search"
+                aria-label="Search by name"
+                aria-describedby="basic-addon2"
+                size={"lg"}
+            />
+
+            <DropdownButton
+                as={InputGroup.Append}
+                variant="outline-primary"
+                title="Search"
+                id="igd"
+            >
+              <Dropdown.Item href="/">Search Author</Dropdown.Item>
+              <Dropdown.Item href="/">Search Book</Dropdown.Item>
+              <Dropdown.Item href="/">Search All</Dropdown.Item>
+            </DropdownButton>
+        </InputGroup>
+        </div>
+      </div>
   );
 }
 
